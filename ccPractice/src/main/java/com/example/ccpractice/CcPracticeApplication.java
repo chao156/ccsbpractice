2bb4fe4ba68d0440226a51c168b9b1ef38b3b6fe
chaochao <457803757@qq.com>
2022-02-22 17:27:43 +0800

    个人练手vue-springboot的springboot项目

---
+++ ccPractice/src/main/java/com/example/ccpractice/CcPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ccpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcPracticeApplication.class, args);
+    }
+
+}
---
+++ ccPractice/src/test/java/com/example/ccpractice/CcPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ccpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
