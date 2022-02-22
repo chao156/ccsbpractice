aafcd718e7c548cc6d79b76142cedb725a162866
qc <quchao199705@163.com>
2022-02-22 21:31:50 +0800

    添加实体类

---
+++ ccPractice/src/main/java/com/example/ccpractice/entity/Student.java
@@ -0,0 +1,40 @@
+package com.example.ccpractice.entity;
+
+public class Student {
+    private Integer id;
+    private Integer age;
+    private String sex;
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
